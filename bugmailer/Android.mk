6941321678290623eb61451cc440d89fe4ff68bf
Simon Busch <morphis@gravedo.de>
2014-02-26 17:41:25 +0100

    Disable building bugmailer
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- bugmailer/Android.mk
+++ bugmailer/Android.mk
@@ -2,6 +2,8 @@
 #
 LOCAL_PATH:= $(call my-dir)
 
+ifeq (true, false)
+
 ifeq (platform,$(TARGET_BUILD_JAVA_SUPPORT_LEVEL))
 include $(CLEAR_VARS)
 
@@ -10,3 +12,5 @@ LOCAL_MODULE := send_bug
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_JAVA_LIBRARY)
 endif #JAVA_SUPPORT
+
+endif
